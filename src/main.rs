66926d4a6046c0435a62779c7280b6966b5923ea
dietvin <vdietric@students.uni-mainz.de>
2025-02-27 08:45:18 +0100

    Added some example code for retrieving bam reads

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,15 @@ mod refinement;
 mod combined_data;
 
 
-use loader::{pod5};
+use loader::{pod5, bam};
 
 fn main() {
     let path: &str = "example_data/can_reads.pod5";
     let paths = vec![path.to_string()];
     let index = pod5::Pod5Index::from_files(&paths).unwrap();
+
+    let path = "example_data/can_mappings.bam";
+    let mut bam_file = bam::BamFileLazy::new(path).unwrap();
 
     for file in index.files() {
         if let Ok(file) = file {
@@ -19,7 +22,15 @@ fn main() {
                 println!("{}", read.signal().len());
                 println!("{}", read.num_samples());
                 println!("{:?}", read.calibration_offset());
-                println!("{:?}\n", read.calibration_scale());
+                println!("{:?}", read.calibration_scale());
+
+                println!("Loading corresponding bam read:");
+                let bam_read = bam_file.get(read.read_id()).unwrap();
+                println!("{}", bam_read.is_mapped());
+                println!("{}\n", bam_read.stride());
+
+
+
             }
         } else {
             println!("Failed to read File")
