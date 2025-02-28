f6b9093de6095a1bd27db4fe05b7e9b5fac05671
dietvin <vdietric@students.uni-mainz.de>
2025-02-28 09:54:02 +0100

    Added docstring

--- src/alignment/query_to_signal.rs
+++ src/alignment/query_to_signal.rs
@@ -1,5 +1,30 @@
 use super::super::error::alignment_errors::query_to_signal_errors::QueryToSignalError;
 
+/// Align the query (base-called) sequence to the signal.
+/// 
+/// This function aligns the query sequence to the signal using the move table generated 
+/// during base-calling. The move table indicates whether a the sequence moved to the next 
+/// base during sequencing.
+/// 
+/// # Arguments
+/// 
+/// * `move_table` - A slice of boolean values indicating if a step forward is taking place
+///                  at a given signal index.
+/// * `stride` - The step size to use when mapping positions between query and signal.
+/// * `signal_len` - The number of measurement in the signal
+/// * `reverse_signal` - Whether to reverse the mapping (True for direct RNA data that runs 
+///                      3'->5' through the pore)
+/// * `query_length` - The length of the base-called sequence
+/// 
+/// # Returns
+///
+/// * `Ok(Vec<usize>)` - A vector mapping query positions to signal positions.
+/// * `Err(QueryToSignalError)` - An error if the mapping is inconsistent with query or signal dimensions.
+///
+/// # Errors
+///
+/// * `QueryToSignalError::DiscordantToSequence` - If the number of steps in the mapping doesn't match the query length.
+/// * `QueryToSignalError::DiscordantToSignal` - If the move table length is inconsistent with signal length and stride.
 pub fn align_query_to_signal(
     move_table: &[bool],
     stride: usize, 
