8745e0915ac612c16d4760c1c0e66eca4a01df6a
dietvin <vdietric@students.uni-mainz.de>
2025-03-28 15:54:38 +0100

    Started implementing the refinement function

--- src/error/refinement_errors/refine_errors.rs
+++ src/error/refinement_errors/refine_errors.rs
@@ -1,4 +1,10 @@
+use super::band_errors::{SequenceBandError, SignalBandError};
+
 #[derive(Debug, thiserror::Error)]
 pub enum RefineError {
+    #[error("Signal band error: {0}")]
+    SignalBandError(#[from] SignalBandError),
+    #[error("Sequence band error: {0}")]
+    SequenceBandError(#[from] SequenceBandError)
 
 }
--- src/refinement/refinement_core/refinement.rs
+++ src/refinement/refinement_core/refinement.rs
@@ -1,4 +1,4 @@
-use crate::error::refinement_errors::refine_errors::RefineError;
+use crate::{error::refinement_errors::refine_errors::RefineError, refinement::refinement_core::bands::Band};
 use super::super::signal_map_refiner::settings::RefineSettings;
 
 pub fn refinement(
@@ -7,5 +7,26 @@ pub fn refinement(
     expected_levels: &Vec<f32>,
     settings: &RefineSettings
 ) -> Result<Vec<usize>, RefineError> {
-    todo!()
+    // trim the signal and adjust the boundaries in the map so it starts at signal index 0
+    let sig_map_start = signal_to_sequence_map[0];
+    let sig_map_end = signal_to_sequence_map[signal_to_sequence_map.len() - 1];
+
+    let signal_trimmed = &signal[sig_map_start..sig_map_end];
+    let mut signal_to_sequence_map_zeroed = signal_to_sequence_map
+        .iter()
+        .map(|el| el-sig_map_start)
+        .collect::<Vec<usize>>();
+
+    let mut band = Band::compute_signal_band(
+        &signal_to_sequence_map_zeroed,
+        expected_levels.len(),
+        *settings.half_bandwidth(),
+        true
+    )?;
+
+    band.convert_to_sequence_band()?;
+
+    Ok(
+        signal_to_sequence_map.iter().map(|el| el + sig_map_start).collect::<Vec<usize>>()
+    )
 }
