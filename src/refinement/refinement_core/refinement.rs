290821b32489b3fb4bffe8ce24ab529975fdd965
dietvin <vdietric@students.uni-mainz.de>
2025-04-01 12:29:10 +0200

    Removed mut

--- src/refinement/refinement_core/refinement.rs
+++ src/refinement/refinement_core/refinement.rs
@@ -12,7 +12,7 @@ pub fn refinement(
     let sig_map_end = signal_to_sequence_map[signal_to_sequence_map.len() - 1];
 
     let signal_trimmed = &signal[sig_map_start..sig_map_end];
-    let mut signal_to_sequence_map_zeroed = signal_to_sequence_map
+    let signal_to_sequence_map_zeroed = signal_to_sequence_map
         .iter()
         .map(|el| el-sig_map_start)
         .collect::<Vec<usize>>();
