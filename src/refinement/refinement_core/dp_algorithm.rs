7e131a96b0ab972e4ceade0acf3265a6be6729ec
dietvin <vdietric@students.uni-mainz.de>
2025-04-01 12:28:40 +0200

    Fixed issue with empty initialization & made mods public for testing

--- src/refinement/refinement_core/dp_algorithm.rs
+++ src/refinement/refinement_core/dp_algorithm.rs
@@ -1,6 +1,6 @@
-mod forward_pass;
+pub mod forward_pass;
 mod traceback;
-mod forward_step;
+pub mod forward_step;
 mod forward_step_dwell_penalty;
 
 use forward_pass::forward_pass;
@@ -26,8 +26,11 @@ pub fn banded_dp(
 
     let band_len = offset_cumsum;
 
-    let mut all_scores: Vec<f32> = Vec::with_capacity(band_len);
-    let mut traceback: Vec<i32> = Vec::with_capacity(band_len);
+    // let mut all_scores: Vec<f32> = Vec::with_capacity(band_len);
+    // let mut traceback: Vec<i32> = Vec::with_capacity(band_len);
+
+    let mut all_scores = vec![f32::INFINITY; band_len];
+    let mut traceback = vec![0; band_len];
 
     forward_pass(
         &mut all_scores,
@@ -39,7 +42,8 @@ pub fn banded_dp(
         method
     );
 
-    let mut path: Vec<usize> = Vec::with_capacity(levels.len()+1);
+    // let mut path: Vec<usize> = Vec::with_capacity(levels.len()+1);
+    let mut path: Vec<usize> = vec![0; levels.len()+1];
     banded_traceback(
         &mut path, 
         band, 
