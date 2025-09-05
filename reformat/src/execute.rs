e25d713259484cb69b727fd6ee0a3296116c8e76
dietvin <vdietric@students.uni-mainz.de>
2025-09-05 11:44:37 +0200

    Added entry function for the reformat library

--- reformat/src/execute.rs
+++ reformat/src/execute.rs
@@ -1,2 +1,22 @@
+use clap::ArgMatches;
+use console::style;
+
+use crate::execute::config::ConfigReformat;
+
 pub mod init_cli;
 pub(crate) mod config;
+
+pub fn execute(input_args: &ArgMatches) {
+    let config = match ConfigReformat::from_argmatches(input_args) {
+        Ok(c) => c,
+        Err(e) => {
+            eprintln!(
+                "Failed to parse input data: {}",
+                format!("{}", style(e).red())
+            );
+            std::process::exit(1);
+        }
+    };
+
+    println!("{:?}",config);
+}
